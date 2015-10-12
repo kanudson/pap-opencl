0211cf0a82295efda087080db836104c2f301d4c
Paul Thieme <kanudson@gmail.com>
2015-10-12 18:13:49 +0200

    [code] CppCL Header/Code
    Ausgangsbasis fuer den ganzne OpenCL krams

---
+++ src/cppcl.hpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cppcl.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/12/2015 10:21:05 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Paul Thieme
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef  cppcl_INC
+#define  cppcl_INC
+
+#include <string>
+#include <vector>
+
+#include <cstdlib>
+
+#include <CL/cl.h>
+
+namespace ocl
+{
+//  helper functions
+std::string get_clPlatformInfo(cl_platform_id platform, cl_platform_info info);
+
+cl_uint getPlatformCount();
+std::vector<cl_platform_id> getPlatformList();
+}   //  namespace ocl
+
+#endif   /* ----- #ifndef cppcl_INC  ----- */
+
